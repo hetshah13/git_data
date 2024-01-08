cdb41f0644bf45669b70c788e79a8a720c8b4b62
hetshah13 <hetshah1309@gmail.com>
2024-01-08 16:41:41 +0530

    2nd commit

--- het.py
+++ het.py
@@ -3,19 +3,16 @@ from datetime import datetime, timezone
 import sqlalchemy as db
 from sqlalchemy.orm import sessionmaker
 
-file_loc = ['D://task_data//uber-tlc-foil-response//uber-trip-data//uber-raw-data-apr14.csv',
-            'D://task_data//uber-tlc-foil-response//uber-trip-data//uber-raw-data-aug14.csv',
-            'D://task_data//uber-tlc-foil-response//uber-trip-data//uber-raw-data-jul14.csv',
-            'D://task_data//uber-tlc-foil-response//uber-trip-data//uber-raw-data-jun14.csv',
-            'D://task_data//uber-tlc-foil-response//uber-trip-data//uber-raw-data-may14.csv',
-            'D://task_data//uber-tlc-foil-response//uber-trip-data//uber-raw-data-sep14.csv']
+file_loc = ['data/uber-raw-data-apr14.csv','data/uber-raw-data-aug14.csv',
+            'data/uber-raw-data-jul14.csv','data/uber-raw-data-jun14.csv',
+            'data/uber-raw-data-may14.csv','data/uber-raw-data-sep14.csv']
 
 dfs = [dd.read_csv(file) for file in file_loc]
 #print(dfs)
 # date:string  Lat:float64	Lon:float64	Base_code:string
 
-l15 = dd.read_csv(r'D:\task_data\uber-tlc-foil-response\uber-trip-data\uber-raw-data-janjune-15.csv\uber-raw-data-janjune-15.csv')
-l1 = dd.read_csv(r"D:\task_data\uber-tlc-foil-response\uber-trip-data\taxi-zone-lookup.csv")
+l15 = dd.read_csv(r'data/uber-raw-data-janjune-15.csv/uber-raw-data-janjune-15.csv')
+l1 = dd.read_csv(r"data/taxi-zone-lookup.csv")
 
 #concatinating files
 df = dd.concat(dfs)
@@ -34,21 +31,11 @@ result['Pickup_date'] = dd.to_datetime(result['Pickup_date'])
 df['Date/Time'] = df['Date/Time'].dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata')
 result['Pickup_date'] = result['Pickup_date'].dt.tz_localize('UTC').dt.tz_convert('Asia/Kolkata')
 
-#df.head()
-#result.head()
-
 #tweeking :replacing base code with base name
 
 base_name= {
-    'B02512': 'Unter',
-    'B02598': 'Hinter',
-    'B02617': 'Weiter',
-    'B02682': 'Schmecken',
-    'B02764': 'Danach-NY',
-    'B02765': 'Grun',
-    'B02835': 'Dreist',
-    'B02836': 'Drinnen'
-}
+    'B02512': 'Unter','B02598': 'Hinter','B02617': 'Weiter','B02682': 'Schmecken',
+    'B02764': 'Danach-NY','B02765': 'Grun','B02835': 'Dreist','B02836': 'Drinnen'}
 
 df['Base'] = df['Base'].replace(base_name)
 result['Dispatching_base_num'] = result['Dispatching_base_num'].replace(base_name)
@@ -59,11 +46,9 @@ engine = db.create_engine('mysql+mysqlconnector://root:hetshah13@localhost:3307/
 # testing a connection
 conn = engine.connect()
 metadata = db.MetaData()
-
 print(repr(metadata))
 
 Session = sessionmaker(bind=engine)
-
 # Create a session
 session = Session()
 try:
